0fa2b93cdf9d4ea1e36f59be778bac502fb411e6
Sergio Diniz <sergiodinizsh@gmail.com>
2016-11-13 08:43:37 -0300

    creating contact DAO, add function ok

--- src/connection/TestConnection.java
+++ src/connection/TestConnection.java
@@ -2,14 +2,32 @@ package connection;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Calendar;
+
+import beans.Contato;
+import dao.ContatoDAO;
 
 public class TestConnection {
 
 	public static void main(String[] args) throws SQLException {
 		// TODO Auto-generated method stub
-		Connection con = new ConnectionJDBC().createConnection();
-		con.close();
-		System.out.println("Connection closed!");
+//		Connection con = new ConnectionJDBC().createConnection();
+//		con.close();
+//		System.out.println("Connection closed!");
+		
+		Contato contato = new Contato();
+		contato.setNome("Sergio");
+		contato.setEmail("sergio2@g.com");
+		contato.setEndereco("rua pedro muniz de brito");
+		contato.setDataNascimento(Calendar.getInstance());
+		
+		
+		ContatoDAO cd = new ContatoDAO();
+		
+		cd.adicionar(contato);
+		
+		
+		
 	}
 
 }
---
+++ src/dao/ContatoDAO.java
@@ -0,0 +1,42 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import beans.Contato;
+import connection.ConnectionJDBC;
+
+public class ContatoDAO {
+
+	private Connection con;
+
+	public ContatoDAO() throws SQLException {
+		this.con = new ConnectionJDBC().createConnection();
+	}
+	
+	
+	public void adicionar(Contato contato) throws SQLException{
+		String sql = "insert into contatos (nome, email, endereco, data_nascimento) values (?,?,?,?)";
+		
+		try {
+			PreparedStatement stat = con.prepareStatement(sql);
+			
+			stat.setString(1, contato.getNome());
+			stat.setString(2, contato.getEmail());
+			stat.setString(3, contato.getEndereco());
+			stat.setDate(4, new Date(contato.getDataNascimento().getTimeInMillis()));
+			
+			stat.execute();
+			System.out.println("Adicionado!");
+			
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		} finally {
+			con.close();
+		}
+	}
+	
+	
+}
